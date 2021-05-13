b1a99418a2414a858bbb247035812005d40ec80f
Anil Kumar <anil_kumarbly@hotmail.com>
2021-05-13 19:26:06 +0530

    Comitt Customer Details

--- CSRPulse.Model/CustomerLicenseActivation.cs
+++ CSRPulse.Model/CustomerLicenseActivation.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+
 
 namespace CSRPulse.Model
 {
-   public class CustomerLicenseActivation:BaseModel
+    public class CustomerLicenseActivation : BaseModel
     {
+        [Display(Name = "License Activation Id")]
         public int LicenceActID { get; set; }
+        [Display(Name = "Customer Id")]
         public int CustomerID { get; set; }
+        [Display(Name = "Plan Id")]
         public int PlanID { get; set; }
+        [Display(Name = "Activation Count")]
         public int ActivationCount { get; set; }
+        [Display(Name = "Activation Date")]
         public DateTime ActivationDate { get; set; }
+        [Display(Name = "Activation Last Date")]
         public DateTime LastActivationDate { get; set; }
         public int PaymentId { get; set; }
     }
--- CSRPulse.Model/CustomerPayment.cs
+++ CSRPulse.Model/CustomerPayment.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CSRPulse.Model
 {
-   public class CustomerPayment:BaseModel
+    public class CustomerPayment : BaseModel
     {
+        [Display(Name = "Payment Id")]
         public int PaymentID { get; set; }
+        [Display(Name = "Customer Id")]
         public int CustomerID { get; set; }
+        [Display(Name = "Amount Paid")]
         public decimal AmountPaid { get; set; }
+        [Display(Name = "Payment Mode")]
         public int PaymentMode { get; set; }
+        [Display(Name = "Paymane Date")]
         public DateTime PaymentDate { get; set; }
+        [Display(Name = "Is Success")]
         public bool IsSuccess { get; set; }
     }
 }
