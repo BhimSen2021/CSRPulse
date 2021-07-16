91ed58e3889904fa52f8bab7ffb5ea901ec99357
Anil Kumar <anil_kumarbly@hotmail.com>
2021-07-16 14:02:35 +0530

    Create ProcessSetup Service

--- CSRPulse.Model/ProcessWorkFlow/ProcessSetup.cs
+++ CSRPulse.Model/ProcessWorkFlow/ProcessSetup.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CSRPulse.Model.ProcessWorkFlow
+namespace CSRPulse.Model
 {
-    public class ProcessSetup
+    public class ProcessSetup : BaseModel
     {
         public int SetupId { get; set; }
         public int ProcessId { get; set; }
@@ -15,11 +15,6 @@ namespace CSRPulse.Model.ProcessWorkFlow
         public int? QuaternaryRoleId { get; set; }
         public int LevelId { get; set; }
         public int? Sequence { get; set; }
-        public bool Skip { get; set; }
-        public bool IsDeleted { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public int CreatedBy { get; set; }
-        public int? Updatedby { get; set; }
-        public DateTime? UpdatedDate { get; set; }
+        public bool Skip { get; set; }       
     }
 }
