d2a40801fa2a9ffa9a1bd91db8c114e6415da92b
Bhim Sen Rai <bhimsenrai76@gmail.com>
2021-10-14 09:58:38 +0530

    Commited Role Rigth Changes

---
+++ CSRPulse/Controllers/RoleRightsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSRPulse.Services.IServices;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CSRPulse.Controllers
+{
+    public class RoleRightsController : BaseController<RoleRightsController>
+    {
+        private readonly IDropdownBindService _ddlService;
+        public RoleRightsController(IDropdownBindService dropdownBindService)
+        {
+            _ddlService = dropdownBindService;
+        }
+        public IActionResult Index()
+        {
+            BindDropdowns();
+            return View();
+        }
+
+        [NonAction]
+        void BindDropdowns()
+        {
+            var roleList = _ddlService.GetRole(null);
+            ViewBag.ddlRole = new SelectList(roleList, "id", "value");
+        }
+    }
+}
